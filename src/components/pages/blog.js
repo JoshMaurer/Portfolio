2dc0baa7fe2b9014262b427da8666d6545e6d460
JoshMaurer <115728247+JoshMaurer@users.noreply.github.com>
2022-12-07 11:42:36 -0600

    07Dec2022 commit 1

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,22 +1,43 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import BlogItem from '../blog/blog-item';
 
 class Blog extends Component {
   constructor() {
     super();
+
+    this.state = {
+      blogItems: []
+    };
+
+    this.getBlogItems = this.getBlogItems.bind(this);
+  }
+
+  getBlogItems() {
+    axios
+      .get('https://anturio.devcamp.space/portfolio/portfolio_blogs', {withCredentials: true
+      })
+      .then(response => {
+        this.setState({
+          blogItems: response.data.portfolio_blogs
+        });
+      })
+      .catch(error => {
+        console.log('getBlogItems error', error);
+      });
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
   }
 
   render() {
-    return (
-      <div>
-        <h2>Blog</h2>
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <BlogItem key={blogItem.id} blogItem={blogItem}/>;
+    });
 
-        <div>
-          <Link to="/about-me">Read some more about me</Link>
-        </div>
-      </div>
-
-    );
+    return <div>{blogRecords}</div>
   }
 }
 
