1da707984b3abc721c6c2a60bf6b467c41618bbc
JoshMaurer <115728247+JoshMaurer@users.noreply.github.com>
2022-12-08 17:07:42 -0600

    08Dec2022

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
 import BlogItem from '../blog/blog-item';
+import BlogModal from '../modals/blog-modal';
 
 class Blog extends Component {
   constructor() {
@@ -13,12 +14,34 @@ class Blog extends Component {
       totalCount: 0,
       currentPage: 0,
       isLoading: true,
+      blogModalIsOpen: false,
     };
 
     this.getBlogItems = this.getBlogItems.bind(this);
     this.onScroll = this.onScroll.bind(this);
     window.addEventListener('scroll', this.onScroll, false);
-        
+    this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
+    this.handleModalClose = this.handleModalClose.bind(this);
+    this.handleSuccessfulNewBlogSubmission = this.handleSuccessfulNewBlogSubmission.bind(this);
+  }
+
+  handleSuccessfulNewBlogSubmission(blog) {
+    this.setState({
+      blogModalIsOpen: false,
+      blogItems: [blog].concat(this.state.blogItems)
+    });
+  }
+
+  handleModalClose() {
+    this.setState({
+      blogModalIsOpen: false
+    });
+  }
+
+  handleNewBlogClick() {
+    this.setState({
+      blogModalIsOpen: true
+    });
   }
 
   onScroll() {
@@ -66,11 +89,22 @@ class Blog extends Component {
 
   render() {
     const blogRecords = this.state.blogItems.map(blogItem => {
-      return <BlogItem key={blogItem.id} blogItem={blogItem}/>;
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
     });
 
     return (
       <div className='blog-container'>
+        <BlogModal 
+        handleSuccessfulNewBlogSubmission={this.handleSuccessfulNewBlogSubmission}
+        handleModalClose={this.handleModalClose}
+        modalIsOpen={this.state.blogModalIsOpen} />
+
+        <div className='new-blog-link'>
+          <a onClick={this.handleNewBlogClick}>
+          <FontAwesomeIcon icon="file-circle-plus" />
+
+          </a>
+        </div>
         
         <div className='content-container'>{blogRecords}</div>
     
